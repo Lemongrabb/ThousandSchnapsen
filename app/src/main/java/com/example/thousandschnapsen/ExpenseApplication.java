fd8cd90a9982da801cdceb88f9b6a75a6f622011
jpziomek <ziomek2369@vp.pl>
2019-05-12 21:17:26 +0200

    db+dao

---
+++ app/src/main/java/com/example/thousandschnapsen/ExpenseApplication.java
@@ -0,0 +1,20 @@
+import android.app.Application;
+import android.content.Context;
+
+import com.example.thousandschnapsen.DbSchnapsen.DbManager;
+
+public class ExpenseApplication extends Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+        DbManager.setConfig(context);
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
